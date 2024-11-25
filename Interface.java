fbe556d629c79608fe8fd829c70565d4ec6e73c7
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-11-25 19:44:15 +0100

    Commit 3
    Rework interface menu

--- Interface.java
+++ Interface.java
@@ -1,85 +1,114 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Interface extends JFrame implements ActionListener {
 
 
-    private JPanel pan1 = new JPanel();
-    private JPanel pan2 = new JPanel();
+
     private Image img;
     private BorderLayout bl =  new BorderLayout();
+
+    private JLabel lab_titre  = new JLabel("La bataille navale");
 
     private int size = 2;
     private int size2 = 1;
     private GridLayout gl = new GridLayout(size,size2);
-    private JButton[][] buttons = new JButton[size][size2];
+    private GridBagConstraints gbl =  new GridBagConstraints();
+    private JButton button1 = new JButton("Lancer une nouvelle partie");
+    private JButton button2 = new JButton("Règles du Jeu");
 
 
-    public Interface()  {
+
+    public Interface() throws IOException {
 
 
         // Création de la fenêtre
         this.setTitle("Bataille Navale");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(new Dimension(500, 500));
-        this.setVisible(true);
+
 
 
         // Pannel / BorderLayout
-        pan1.setBackground(new Color(Color.OPAQUE));
-        this.setContentPane(pan1);
-        pan1.setLayout(bl);
-        JLabel labTitre = new JLabel("Bataille navale");
-        Font font=new Font("Fira Code",Font.PLAIN,50);
-        labTitre.setForeground(Color.WHITE);
-        labTitre.setFont(font);
-        labTitre.setHorizontalAlignment(SwingConstants.CENTER);
 
-        // GridLayout
+       JPanel pan = new JPanel(){
+           @Override
+           protected void paintComponent(Graphics g) {
+               super.paintComponent(g);
+               ImageIcon backgroundImage = new ImageIcon("../dm_reseau_bataille/Media/bn2.jpg");
+               g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+           }
+       };
 
-        pan1.add(pan2, bl.SOUTH);
-        pan2.setLayout(gl);
-        pan1.add(labTitre, bl.NORTH);
-        addButtons(pan2);
+       pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
 
+       button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+       button1.setBackground(Color.lightGray);
+       button2.setBackground(Color.lightGray);
+       button1.setFont(new Font("Calibri", Font.ITALIC, 20));
+       button2.setFont(new Font("Calibri", Font.ITALIC, 20));
+
+       button2.setAlignmentX(Component.CENTER_ALIGNMENT);
+       lab_titre.setAlignmentX(Component.CENTER_ALIGNMENT);
+       lab_titre.setForeground(Color.WHITE);
+       lab_titre.setFont(new Font("Calibri", Font.PLAIN, 60));
+
+        // Ajouter les boutons au panneau
+        pan.add(Box.createRigidArea(new Dimension(0, 40)));
+        pan.add(lab_titre);
+        pan.add(Box.createRigidArea(new Dimension(0, 200)));
+        pan.add(button1);
+        pan.add(Box.createRigidArea(new Dimension(0, 20)));
+        pan.add(button2);
+        this.add(pan);
+
+
+
+        
 
         // ActionListener
 
-        buttons[0][0].addActionListener(this);
-        buttons[1][0].addActionListener(this);
+
+
+        String path = "../dm_reseau_bataille/Media/bataille_navale.jpg";
+
+
+       button1.addActionListener(this);
+       button2.addActionListener(this);
+
+
+        this.setVisible(true);
+
+
 
 
 
     }
 
-    public void addButtons(JPanel panel) {
-        for (int k = 0; k < size; k++) {
-            for (int j = 0; j < size2; j++) {
-                buttons[k][j] = new JButton();
-                if(k ==1 ){
-                    buttons[k][j].setText("Jouer");
-                }else{
-                    buttons[k][j].setText("Règles du jeu");
-                }
-                panel.add(buttons[k][j]);
-            }
-        }
-    }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == buttons[0][0]){
+        if(e.getSource() == button2){
             String s  = "La bataille navale est un jeu de stratégie pour deux joueurs. \n" +
                     "Chaque joueur dispose une flotte de navires sur une grille cachée de son adversaire. \n" +
                     "Le but est de deviner la position des navires ennemis en annonçant des coordonnées. \n" +
                     "Chaque tir peut être \"à l’eau\", \"touché\", ou \"coulé\", selon l’impact sur les navires adverses. \n" +
                     "Le vainqueur est le premier à couler tous les navires de l’adversaire.";
             JOptionPane.showMessageDialog(this,s,"Alert",JOptionPane.WARNING_MESSAGE);
-        }else if(e.getSource() == buttons[1][0]){
+        }else if(e.getSource() == button1){
             this.dispose();
             Parametres p = new Parametres(10);
         }
     }
+
+
+
 }
--- Launcher.java
+++ Launcher.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class Launcher {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         // Launch the app
         Interface i = new Interface();
 
