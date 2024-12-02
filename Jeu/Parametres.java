f64d9486f4fc901d8913b7c40d873d3d3dea01dc
MistiXe <37303853+MistiXe@users.noreply.github.com>
2024-12-02 15:07:25 +0100

    Update Parametres.java
    On choisit maintenant des pions de type lettre + chiffre

--- Jeu/Parametres.java
+++ Jeu/Parametres.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,25 +26,19 @@ public class Parametres extends JFrame implements ActionListener {
     private final JPanel pan_main = new JPanel();
     private final BorderLayout bl = new BorderLayout();
     private final JPanel main_panel = new JPanel();
-    private final GridLayout gl_settings = new GridLayout(6, 3);
-    private final Integer[] choix_coord = {null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    private final String[] statement = {"Horizontal", "Vertical"};
+    private final GridLayout gl_settings = new GridLayout(6, 2);
+    private final String[] statement = {null,"Horizontal", "Vertical"};
     private final JLabel nom_du_pion = new JLabel("Porte-Avion (5 Cases) : ");
     private final JLabel type = new JLabel("Type de bateau");
-    private final JLabel type_de_X = new JLabel("Coordonnées en X");
-    private final JLabel type_de_Y = new JLabel("Coordonnées en Y");
+    private final JLabel type_de_X = new JLabel("Coordonnées :");
     private final JLabel type_de_Etat = new JLabel("Orientation");
     private final JLabel nom_du_pion2 = new JLabel("Croiseur (4 Cases) : ");
     private final JLabel nom_du_pion3 = new JLabel("Torpilleur (1 Case) : ");
     private final JLabel nom_du_pion4 = new JLabel("Sous-Marins (2 Cases) : ");
-    private final JComboBox<Integer> LISTE_PION1 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION2 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION3 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION4 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION1_1 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION2_1 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION3_1 = new JComboBox<>(choix_coord);
-    private final JComboBox<Integer> LISTE_PION4_1 = new JComboBox<>(choix_coord);
+    private final JComboBox<String> LISTE_PION1 = new JComboBox<>( generateCoordinates());
+    private final JComboBox<String> LISTE_PION2 = new JComboBox<>( generateCoordinates());
+    private final JComboBox<String> LISTE_PION3 = new JComboBox<>( generateCoordinates());
+    private final JComboBox<String> LISTE_PION4 = new JComboBox<>( generateCoordinates());
     private final JComboBox<String> LISTE_PION_AL1 = new JComboBox<>(statement);
     private final JComboBox<String> LISTE_PION_AL2 = new JComboBox<>(statement);
     private final JComboBox<String> LISTE_PION_AL3 = new JComboBox<>(statement);
@@ -77,34 +72,27 @@ public class Parametres extends JFrame implements ActionListener {
         this.main_panel.setLayout(gl_settings);
         this.main_panel.add(type);
         this.main_panel.add(type_de_X);
-        this.main_panel.add(type_de_Y);
         this.main_panel.add(type_de_Etat);
         this.main_panel.add(nom_du_pion);
         this.main_panel.add(LISTE_PION1);
-        this.main_panel.add(LISTE_PION1_1);
         this.main_panel.add(LISTE_PION_AL1);
         this.main_panel.add(nom_du_pion2);
         this.main_panel.add(LISTE_PION2);
-        this.main_panel.add(LISTE_PION2_1);
         this.main_panel.add(LISTE_PION_AL2);
         this.main_panel.add(nom_du_pion3);
         this.main_panel.add(LISTE_PION3);
-        this.main_panel.add(LISTE_PION3_1);
         this.main_panel.add(LISTE_PION_AL3);
         this.main_panel.add(nom_du_pion4);
         this.main_panel.add(LISTE_PION4);
-        this.main_panel.add(LISTE_PION4_1);
         this.main_panel.add(LISTE_PION_AL4);
+        System.out.println(new ArrayList<>(Arrays.asList(generateCoordinates())));
+
 
 
         liste_combo.add(LISTE_PION1);
-        liste_combo.add(LISTE_PION1_1);
         liste_combo.add(LISTE_PION2);
-        liste_combo.add(LISTE_PION2_1);
         liste_combo.add(LISTE_PION3);
-        liste_combo.add(LISTE_PION3_1);
         liste_combo.add(LISTE_PION4);
-        liste_combo.add(LISTE_PION4_1);
 
         liste_combo_etat.add(LISTE_PION_AL1);
         liste_combo_etat.add(LISTE_PION_AL2);
@@ -161,25 +149,25 @@ public class Parametres extends JFrame implements ActionListener {
         ArrayList<ArrayList<Integer>> coordinates = new ArrayList<>();
 
         // Parcourir les JComboBox par paires pour former des couples (x, y)
-        for (int i = 0; i < liste_combo.size(); i += 2) {
+        for (int i = 0; i < liste_combo.size(); i ++) {
             // Récupérer les éléments sélectionnés dans chaque JComboBox
-            Object xObj = liste_combo.get(i).getSelectedItem();
-            Object yObj = liste_combo.get(i + 1).getSelectedItem();
-
-            // Vérifier que les deux valeurs sont sélectionnées
-            if (xObj == null || yObj == null) {
-                allValuesSelected = false; // Marque qu'une sélection est manquante
+            Object Obj = liste_combo.get(i).getSelectedItem();
+            if (Obj == null) {
+                allValuesSelected = false; // Marquer qu'une sélection est manquante
                 break;
             }
-            // Ajouter le couple (x, y) aux coordonnées
-            int x = (int) xObj;
-            int y = (int) yObj;
+            String coord = (String) Obj;
+            char l = coord.charAt(0);
+            int chiffre = Integer.parseInt(coord.substring(1));
+            int x = l - 'A';
+            int y = chiffre;
             ArrayList<Integer> sous_liste = new ArrayList<>();
             sous_liste.add(x);
             sous_liste.add(y);
+            System.out.println(sous_liste);
             coordinates.add(sous_liste);
             this.liste_combo.get(i).setEnabled(false);
-            this.liste_combo.get(i + 1).setEnabled(false);
+
 
         }
 
@@ -310,6 +298,20 @@ public class Parametres extends JFrame implements ActionListener {
         }
         return valide;
     }
+    public static String[] generateCoordinates() {
+        String[] coordinates = new String[10*10+1];
+        int index = 1;
+        coordinates[0] = null;
+        for (int i = 0; i < 10; i++) {
+            char letter = (char) ('A' + i);
+            for (int j = 0; j < 10; j++) {
+                coordinates[index++] = letter + String.valueOf(j);
+            }
+        }
+        return coordinates;
+    }
+
+
 
 
 }
